c0ddd6951109d5ee45935086687a855eb20457d3
Matt R <mattr@aweber.com>
2014-04-06 19:40:32 -0400

    Add /stream endpoint that takes query param q to specify tweets to look for.
    * Change twitter library to be twit from here: https://github.com/ttezel/twit
    * Filter the current twitter stream using provided query param.
    * Slims down the result JSON before sending to requestor.

--- index.js
+++ index.js
@@ -3,69 +3,54 @@ var express = require("express");
 var util = require("util");
 var logfmt = require("logfmt");
 var app = express();
-var twitterAPI = require('node-twitter-api');
+
 var creds = require("./mycreds");
+
+var twitter = require('twit');
+var twit = new twitter({
+	consumer_key: creds.consumerKey,
+	consumer_secret: creds.consumerSecret,
+	access_token: creds.accessToken,
+	access_token_secret: creds.accessTokenSecret
+});
 
 app.use(logfmt.requestLogger());
 
 var twiRequestToken;
 var twiRequestSecret;
 
-var twitter = new twitterAPI({
-	consumerKey: creds.consumerKey,
-	consumerSecret: creds.consumerSecret,
-	callback: 'http://hashfinder.herokuapp.com/twi-auth'
-});
-
 app.get('/', function(req, res) {
-	response = "Nope!";
-	twitter.getRequestToken(function(error, requestToken, requestTokenSecret, results) {
-		if (error) {
-			var errorString = util.inspect(error, false, null);
-			console.log("Error getting OAuth request token : " + errorString);
-			res.send(errorString);
-		} else {
-			//store token and tokenSecret somewhere, you'll need them later; redirect user
-			twiRequestToken = requestToken;
-			twiRequestSecret = requestTokenSecret;
-			response = "Request Token obtained: " + twiRequestToken + " : " + twiRequestSecret;
-			console.log(response);
-			res.redirect("https://twitter.com/oauth/authenticate?oauth_token=" + twiRequestToken);
-			res.send(response);
-		}
-	});
-
+	res.set('Content-Type', 'application/json');
+	return res.send(app.routes);
 });
 
-app.get('/twi-auth', function(req, res) {
-	console.log("!~@~@~@~@~@~@~@! TWI-AUTH SAY MOO !~@~@~@~@~@~@~@!");
+app.get('/stream', function(req, res) {
+	console.log("starting stream");
+	//
+	// filter the public stream by english tweets containing `#apple`
+	//
+	var result = [];
+	var stream = twit.stream('statuses/filter', {
+		track: req.query.q,
+		language: 'en'
+	})
 
-	if (twiRequestToken == req.query.oauth_token) {
-		twitter.getAccessToken(twiRequestToken, twiRequestSecret, req.query.oauth_verifier,
-			function(error, accessToken, accessTokenSecret, results) {
-				if (error) {
-					console.log(error);
-				} else {
-					//store accessToken and accessTokenSecret somewhere (associated to the user)
-					//Step 4: Verify Credentials belongs here
-					twitter.verifyCredentials(accessToken, accessTokenSecret,
-						function(error, data, response) {
-							if (error) {
-								//something was wrong with either accessToken or accessTokenSecret
-								//start over with Step 1
-							} else {
-								//accessToken and accessTokenSecret can now be used to make api-calls (not yet implemented)
-								//data contains the user-data described in the official Twitter-API-docs
-								//you could e.g. display his screen_name
-								console.log(data["screen_name"]);
-							}
-							res.send("Hello " + data["screen_name"]);
-						});
-				}
-			});
-	} else {
-		res.send("Couldn't help ya, request token mismatch: mine: " + twiRequestToken + " yours: " + req.query.oauth_token);
-	}
+	stream.on('tweet', function(tweet) {
+		var tweetS = util.inspect(tweet);
+		var slimTweet = {};
+		slimTweet.user = tweet.user.screen_name;
+		slimTweet.text = tweet.text;
+		slimTweet.source = tweet.source;
+		result.push(slimTweet);
+		console.log(tweetS);
+		// res.write(tweetS);
+	})
+
+	setTimeout(function() {
+		stream.stop();
+		console.log('stopped stream');
+		res.json(result);
+	}, 5000);
 
 });
 
