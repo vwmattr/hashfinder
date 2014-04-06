325d2e484809ddc48ccf12aec69b32e5e24f17eb
Matt R <mattr@aweber.com>
2014-04-06 14:26:44 -0400

    add a call to twitter's search to the main endpoint.

--- index.js
+++ index.js
@@ -35,46 +35,56 @@ app.get('/', function(req, res) {
 		}
 	});
 
-
 });
 
 app.get('/twi-auth', function(req, res) {
-		console.log("!~@~@~@~@~@~@~@! TWI-AUTH SAY MOO !~@~@~@~@~@~@~@!");
+	console.log("!~@~@~@~@~@~@~@! TWI-AUTH SAY MOO !~@~@~@~@~@~@~@!");
 
-
-
-		if (twiRequestToken == req.query.oauth_token) {
-			twitter.getAccessToken(twiRequestToken, twiRequestSecret, req.query.oauth_verifier,
-				function(error, accessToken, accessTokenSecret, results) {
-					if (error) {
-						console.log(error);
-					} else {
-						//store accessToken and accessTokenSecret somewhere (associated to the user)
-						//Step 4: Verify Credentials belongs here
-						twitter.verifyCredentials(accessToken, accessTokenSecret,
-							function(error, data, response) {
-								if (error) {
-									//something was wrong with either accessToken or accessTokenSecret
-									//start over with Step 1
-								} else {
-									//accessToken and accessTokenSecret can now be used to make api-calls (not yet implemented)
-									//data contains the user-data described in the official Twitter-API-docs
-									//you could e.g. display his screen_name
-									console.log(data["screen_name"]);
-								}
-								res.send("Hello " + data["screen_name"]);
-							});
-						}
-					});
+	if (twiRequestToken == req.query.oauth_token) {
+		twitter.getAccessToken(twiRequestToken, twiRequestSecret, req.query.oauth_verifier,
+			function(error, accessToken, accessTokenSecret, results) {
+				if (error) {
+					console.log(error);
 				} else {
-					res.send("Couldn't help ya, request token mismatch: mine: " + twiRequestToken + " yours: " + req.query.oauth_token);
+					//store accessToken and accessTokenSecret somewhere (associated to the user)
+					//Step 4: Verify Credentials belongs here
+					twitter.verifyCredentials(accessToken, accessTokenSecret,
+						function(error, data, response) {
+							if (error) {
+								//something was wrong with either accessToken or accessTokenSecret
+								//start over with Step 1
+							} else {
+								//accessToken and accessTokenSecret can now be used to make api-calls (not yet implemented)
+								//data contains the user-data described in the official Twitter-API-docs
+								//you could e.g. display his screen_name
+								console.log(data["screen_name"]);
+							}
+
+							twitter.search("tweets", {
+									"q": "googleio"
+								}, accessToken, accessTokenSecret,
+								function(error, data, response) {
+									if (error) {
+										var errorString = util.inspect(error, false, null);
+										console.log("Error getting OAuth request token : " + errorString);
+										res.send(errorString);
+									} else {
+										res.send(util.inspect(data, false, null));
+									}
+								});
+							// res.send("Hello " + data["screen_name"]);
+						});
 				}
-
-
-
 			});
+	} else {
+		res.send("Couldn't help ya, request token mismatch: mine: " + twiRequestToken + " yours: " + req.query.oauth_token);
+	}
 
-		var port = Number(process.env.PORT || 5000);
-		app.listen(port, function() {
-			console.log("Listening on " + port);
-		});
+
+
+});
+
+var port = Number(process.env.PORT || 5000);
+app.listen(port, function() {
+	console.log("Listening on " + port);
+});
